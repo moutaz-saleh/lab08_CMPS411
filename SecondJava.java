f90ad31579bb8d3f1030589428970a845f27f932
moutaz <moutaz.saleh@qu.edu.qa>
2015-04-20 08:39:21 +0300

    verion1

---
+++ SecondJava.java
@@ -0,0 +1,6 @@
+import java.lang.*;  
+  public class SecondJava {  
+   public static void main(String[] args)  
+  {  System.out.println("This is my second Java program."); 
+      StdOut.print("Type the second integer: ");
+  } }  
