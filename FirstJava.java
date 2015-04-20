499f3ddd5c853176e214a3b9d125f052d1c1a4be
moutaz <moutaz.saleh@qu.edu.qa>
2015-04-20 08:11:59 +0300

    my first FirstJava.java

---
+++ FirstJava.java
@@ -0,0 +1,6 @@
+import java.lang.*;  
+  public class FirstJava {  
+   public static void main(String[] args)  
+  {  System.out.println("This is my first Java program."); 
+      StdOut.print("Type the first integer: ");
+   } }
